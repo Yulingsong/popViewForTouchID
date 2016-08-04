261f5d36a1460c46c195c79dbfc1b81a2f03bf29
yulingsong <yulingsong5@126.com>
2016-08-04 19:55:35 +0800

    Initial Commit

---
+++ popViewForTouchID/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  popViewForTouchID
+//
+//  Created by yulingsong on 16/8/4.
+//  Copyright © 2016年 yulingsong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ popViewForTouchID/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  popViewForTouchID
+//
+//  Created by yulingsong on 16/8/4.
+//  Copyright © 2016年 yulingsong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
